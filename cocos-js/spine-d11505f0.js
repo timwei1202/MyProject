52e824ce139628a39ce2549da47ffb0834a79c6e
timwei <tim791202@gmail.com>
2025-03-28 11:25:01 +0800

    Fix import map for GitHub Pages deployment

--- cocos-js/spine-d11505f0.js
+++ cocos-js/spine-d11505f0.js
@@ -1,1 +1,3 @@
-System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/spine-17c81aa0.wasm")}}}));
+version https://git-lfs.github.com/spec/v1
+oid sha256:0cab1ac874bceb6d4d433d2c6d093674bc02b6f19d275161b4b40b8ce9ba0a10
+size 119
--- cocos-js/spine.js-6770127a.js
+++ cocos-js/spine.js-6770127a.js
@@ -1,1 +1,3 @@
-System.register([],(function(e){"use strict";return{execute:function(){e("default","assets/spine.js.mem-dc937d48.bin")}}}));
+version https://git-lfs.github.com/spec/v1
+oid sha256:8c6cf20bd5178faf3615c58525f799d86c7c4f6b5ec00a5c42ed658ada4c4022
+size 125
